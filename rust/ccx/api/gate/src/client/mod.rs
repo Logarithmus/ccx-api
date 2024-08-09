d39a5f3666c2e57728c876f3ea87b3d1473d0800
Artur Sinila <a.sinila@lunu.io>
2024-08-09 20:57:22 +0200

    feat: start websocket implementation for gate

--- rust/ccx/api/gate/src/client/mod.rs
+++ rust/ccx/api/gate/src/client/mod.rs
@@ -2,6 +2,7 @@ pub mod config;
 // pub mod nonce;
 pub mod rest;
 pub mod signer;
+pub mod websocket;
 
 // pub use nonce::Nonce;
 pub use signer::GateApiCred;
--- rust/ccx/api/gate/src/lib.rs
+++ rust/ccx/api/gate/src/lib.rs
@@ -3,6 +3,8 @@ extern crate core;
 pub mod api;
 #[cfg(feature = "with_network")]
 pub mod client;
+#[cfg(feature = "with_network")]
+pub mod websocket;
 
 #[cfg(feature = "with_network")]
 pub use self::with_network::*;
@@ -10,4 +12,5 @@ pub use self::with_network::*;
 #[cfg(feature = "with_network")]
 mod with_network {
     pub use super::api::GateApi;
+
 }
